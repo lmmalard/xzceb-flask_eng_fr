1950039d2edeb1cafa866a0c9be653965f671b8d
lmmalard <134538191+lmmalard@users.noreply.github.com>
2023-06-01 11:36:53 -0600

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,1 +1,9 @@
+from deep_translator import MyMemoryTranslator
 
+def english_To_french(EnglishText):
+    #write the code here
+    return FrenchText
+
+def french_To_english(FrenchText):
+    #write the code here
+    return EnglishText
